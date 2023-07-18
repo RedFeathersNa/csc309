d919114ef3904e4a465b34e1557e00dfe91d21eb
RedFeathersNa <139867369+RedFeathersNa@users.noreply.github.com>
2023-07-18 14:55:16 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,31 +1,35 @@
+
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = '';
     $db = 'csc309';
 
     // Open a new connection
     $con = new mysqli($server, $username, $password, $db);
+
 
     // Check connection
     if ($con->connect_error){
         die("Connection failed: " . $con->connect_error);
     }
 
-    // Query
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('$firstname', '$lastname', '$email')";
+    // Create a Table
+    $sql = 'CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50) NOT NULL UNIQUE,
+        gender CHAR(1) NOT NULL,
+        date_of_birth DATE NOT NULL
+    )';
 
     $result = $con->query($sql);
 
     if ($result === true) {
-        echo 'Record inserted successfully';
+        echo 'Table created successfully';
     } else {
-        echo 'Error inserting record: ' . $con->error;
+        echo 'Error creating creating2: ' . $con->error;
     }
 
     // Close connection
